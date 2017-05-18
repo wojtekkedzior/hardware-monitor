8ff6b27a8de7af1dd6921a65c6252064bd0797cf
wojtek <wowaked@gmail.com>
2017-05-18 20:39:33 +0200

    refactoring

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -69,8 +69,12 @@ void MainWindow::onProgressChanged(std::map<std::string, std::string> map) {
      ui->fan6_current->setText(QString::number(fanRpm));
 }
 
-void updateSensorValues (QTextBrowser* current, QTextBrowser* min, QTextBrowser* max, QTextBrowser* extraReadingOne, QTextBrowser* extraReadingTwo, int reading, int extraReadingOne_value, int extraReadingTwo_value) {
+void updateSensorValues (QTextBrowser* current, QTextBrowser* min, QTextBrowser* max, QTextBrowser* extraReadingOne, QTextBrowser* extraReadingTwo,
+                         string str_reading, string str_extraReadingOne_value, string str_extraReadingTwo_value) {
     int _min, _max;
+    int reading = stoi(str_reading);
+    int extraReadingOne_value = stoi(str_extraReadingOne_value);
+    int extraReadingTwo_value = stoi(str_extraReadingTwo_value);
 
     current->setText(QString::number(reading/1000));
 
@@ -103,101 +107,101 @@ void updateSensorValues (QTextBrowser* current, QTextBrowser* min, QTextBrowser*
 
 void MainWindow::cpuFrqChanged(std::map<std::string, std::string> map) {
     updateSensorValues(ui->cpu0_current, ui->cpu0_min, ui->cpu0_max, ui->cpu0_max_allowed, ui->cpu0_min_allowed,
-        stoi(map["/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq"]));
+        map["/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq"],
+        map["/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq"],
+        map["/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq"]);
 
     updateSensorValues(ui->cpu1_current, ui->cpu1_min, ui->cpu1_max, ui->cpu1_max_allowed, ui->cpu1_min_allowed,
-        stoi(map["/sys/devices/system/cpu/cpu1/cpufreq/cpuinfo_cur_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu1/cpufreq/cpuinfo_max_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu1/cpufreq/cpuinfo_min_freq"]));
+        map["/sys/devices/system/cpu/cpu1/cpufreq/cpuinfo_cur_freq"],
+        map["/sys/devices/system/cpu/cpu1/cpufreq/cpuinfo_max_freq"],
+        map["/sys/devices/system/cpu/cpu1/cpufreq/cpuinfo_min_freq"]);
 
     updateSensorValues(ui->cpu2_current, ui->cpu2_min, ui->cpu2_max, ui->cpu2_max_allowed, ui->cpu2_min_allowed,
-        stoi(map["/sys/devices/system/cpu/cpu2/cpufreq/cpuinfo_cur_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu2/cpufreq/cpuinfo_max_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu2/cpufreq/cpuinfo_min_freq"]));
+        map["/sys/devices/system/cpu/cpu2/cpufreq/cpuinfo_cur_freq"],
+        map["/sys/devices/system/cpu/cpu2/cpufreq/cpuinfo_max_freq"],
+        map["/sys/devices/system/cpu/cpu2/cpufreq/cpuinfo_min_freq"]);
 
     updateSensorValues(ui->cpu3_current, ui->cpu3_min, ui->cpu3_max, ui->cpu3_max_allowed, ui->cpu3_min_allowed,
-        stoi(map["/sys/devices/system/cpu/cpu3/cpufreq/cpuinfo_cur_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu3/cpufreq/cpuinfo_max_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu3/cpufreq/cpuinfo_min_freq"]));
+        map["/sys/devices/system/cpu/cpu3/cpufreq/cpuinfo_cur_freq"],
+        map["/sys/devices/system/cpu/cpu3/cpufreq/cpuinfo_max_freq"],
+        map["/sys/devices/system/cpu/cpu3/cpufreq/cpuinfo_min_freq"]);
 
     updateSensorValues(ui->cpu4_current, ui->cpu4_min, ui->cpu4_max, ui->cpu4_max_allowed, ui->cpu4_min_allowed,
-        stoi(map["/sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_cur_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_max_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_min_freq"]));
+        map["/sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_cur_freq"],
+        map["/sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_max_freq"],
+        map["/sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_min_freq"]);
 
     updateSensorValues(ui->cpu5_current, ui->cpu5_min, ui->cpu5_max, ui->cpu5_max_allowed, ui->cpu5_min_allowed,
-        stoi(map["/sys/devices/system/cpu/cpu5/cpufreq/cpuinfo_cur_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu5/cpufreq/cpuinfo_max_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu5/cpufreq/cpuinfo_min_freq"]));
+        map["/sys/devices/system/cpu/cpu5/cpufreq/cpuinfo_cur_freq"],
+        map["/sys/devices/system/cpu/cpu5/cpufreq/cpuinfo_max_freq"],
+        map["/sys/devices/system/cpu/cpu5/cpufreq/cpuinfo_min_freq"]);
 
     updateSensorValues(ui->cpu6_current, ui->cpu6_min, ui->cpu6_max, ui->cpu6_max_allowed, ui->cpu6_min_allowed,
-        stoi(map["/sys/devices/system/cpu/cpu6/cpufreq/cpuinfo_cur_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu6/cpufreq/cpuinfo_max_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu6/cpufreq/cpuinfo_min_freq"]));
+        map["/sys/devices/system/cpu/cpu6/cpufreq/cpuinfo_cur_freq"],
+        map["/sys/devices/system/cpu/cpu6/cpufreq/cpuinfo_max_freq"],
+        map["/sys/devices/system/cpu/cpu6/cpufreq/cpuinfo_min_freq"]);
 
     updateSensorValues(ui->cpu7_current, ui->cpu7_min, ui->cpu7_max, ui->cpu7_max_allowed, ui->cpu7_min_allowed,
-        stoi(map["/sys/devices/system/cpu/cpu7/cpufreq/cpuinfo_cur_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu7/cpufreq/cpuinfo_max_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu7/cpufreq/cpuinfo_min_freq"]));
+        map["/sys/devices/system/cpu/cpu7/cpufreq/cpuinfo_cur_freq"],
+        map["/sys/devices/system/cpu/cpu7/cpufreq/cpuinfo_max_freq"],
+        map["/sys/devices/system/cpu/cpu7/cpufreq/cpuinfo_min_freq"]);
 
     updateSensorValues(ui->cpu8_current, ui->cpu8_min, ui->cpu8_max, ui->cpu8_max_allowed, ui->cpu8_min_allowed,
-        stoi(map["/sys/devices/system/cpu/cpu8/cpufreq/cpuinfo_cur_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu8/cpufreq/cpuinfo_max_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu8/cpufreq/cpuinfo_min_freq"]));
+        map["/sys/devices/system/cpu/cpu8/cpufreq/cpuinfo_cur_freq"],
+        map["/sys/devices/system/cpu/cpu8/cpufreq/cpuinfo_max_freq"],
+        map["/sys/devices/system/cpu/cpu8/cpufreq/cpuinfo_min_freq"]);
 
     updateSensorValues(ui->cpu9_current, ui->cpu9_min, ui->cpu9_max, ui->cpu9_max_allowed, ui->cpu9_min_allowed,
-        stoi(map["/sys/devices/system/cpu/cpu9/cpufreq/cpuinfo_cur_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu9/cpufreq/cpuinfo_max_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu9/cpufreq/cpuinfo_min_freq"]));
+        map["/sys/devices/system/cpu/cpu9/cpufreq/cpuinfo_cur_freq"],
+        map["/sys/devices/system/cpu/cpu9/cpufreq/cpuinfo_max_freq"],
+        map["/sys/devices/system/cpu/cpu9/cpufreq/cpuinfo_min_freq"]);
 
     updateSensorValues(ui->cpu10_current, ui->cpu10_min, ui->cpu10_max, ui->cpu10_max_allowed, ui->cpu10_min_allowed,
-        stoi(map["/sys/devices/system/cpu/cpu10/cpufreq/cpuinfo_cur_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu10/cpufreq/cpuinfo_max_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu10/cpufreq/cpuinfo_min_freq"]));
+        map["/sys/devices/system/cpu/cpu10/cpufreq/cpuinfo_cur_freq"],
+        map["/sys/devices/system/cpu/cpu10/cpufreq/cpuinfo_max_freq"],
+        map["/sys/devices/system/cpu/cpu10/cpufreq/cpuinfo_min_freq"]);
 
     updateSensorValues(ui->cpu11_current, ui->cpu11_min, ui->cpu11_max, ui->cpu11_max_allowed, ui->cpu11_min_allowed,
-        stoi(map["/sys/devices/system/cpu/cpu11/cpufreq/cpuinfo_cur_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu11/cpufreq/cpuinfo_max_freq"]),
-        stoi(map["/sys/devices/system/cpu/cpu11/cpufreq/cpuinfo_min_freq"]));
+        map["/sys/devices/system/cpu/cpu11/cpufreq/cpuinfo_cur_freq"],
+        map["/sys/devices/system/cpu/cpu11/cpufreq/cpuinfo_max_freq"],
+        map["/sys/devices/system/cpu/cpu11/cpufreq/cpuinfo_min_freq"]);
 }
 
 void MainWindow::cpuTempChanged(std::map<std::string, std::string> map) {
     updateSensorValues(ui->package_current, ui->package_min, ui->package_max, ui->package_max_allowed, ui->package_critical,
-        stoi(map["/sys/class/hwmon/hwmon0/temp1_input"]),
-        stoi(map["/sys/class/hwmon/hwmon0/temp1_max"]),
-        stoi(map["/sys/class/hwmon/hwmon0/temp1_crit"]));
+        map["/sys/class/hwmon/hwmon0/temp1_input"],
+        map["/sys/class/hwmon/hwmon0/temp1_max"],
+        map["/sys/class/hwmon/hwmon0/temp1_crit"]);
 
     updateSensorValues(ui->cpu0_temp_current, ui->cpu0_temp_min, ui->cpu0_temp_max, ui->cpu0_temp_max_allowed, ui->cpu0_temp_critical,
-        stoi(map["/sys/class/hwmon/hwmon0/temp2_input"]),
-        stoi(map["/sys/class/hwmon/hwmon0/temp2_max"]),
-        stoi(map["/sys/class/hwmon/hwmon0/temp2_crit"]));
+        map["/sys/class/hwmon/hwmon0/temp2_input"],
+        map["/sys/class/hwmon/hwmon0/temp2_max"],
+        map["/sys/class/hwmon/hwmon0/temp2_crit"]);
 
     updateSensorValues(ui->cpu1_temp_current, ui->cpu1_temp_min, ui->cpu1_temp_max, ui->cpu1_temp_max_allowed, ui->cpu1_temp_critical,
-        stoi(map["/sys/class/hwmon/hwmon0/temp3_input"]),
-        stoi(map["/sys/class/hwmon/hwmon0/temp3_max"]),
-        stoi(map["/sys/class/hwmon/hwmon0/temp3_crit"]));
+        map["/sys/class/hwmon/hwmon0/temp3_input"],
+        map["/sys/class/hwmon/hwmon0/temp3_max"],
+        map["/sys/class/hwmon/hwmon0/temp3_crit"]);
 
     updateSensorValues(ui->cpu2_temp_current, ui->cpu2_temp_min, ui->cpu2_temp_max, ui->cpu2_temp_max_allowed, ui->cpu2_temp_critical,
-        stoi(map["/sys/class/hwmon/hwmon0/temp4_input"]),
-        stoi(map["/sys/class/hwmon/hwmon0/temp4_max"]),
-        stoi(map["/sys/class/hwmon/hwmon0/temp4_crit"]));
+        map["/sys/class/hwmon/hwmon0/temp4_input"],
+        map["/sys/class/hwmon/hwmon0/temp4_max"],
+        map["/sys/class/hwmon/hwmon0/temp4_crit"]);
 
     updateSensorValues(ui->cpu3_temp_current, ui->cpu3_temp_min, ui->cpu3_temp_max, ui->cpu3_temp_max_allowed, ui->cpu3_temp_critical,
-        stoi(map["/sys/class/hwmon/hwmon0/temp5_input"]),
-        stoi(map["/sys/class/hwmon/hwmon0/temp5_max"]),
-        stoi(map["/sys/class/hwmon/hwmon0/temp5_crit"]));
+        map["/sys/class/hwmon/hwmon0/temp5_input"],
+        map["/sys/class/hwmon/hwmon0/temp5_max"],
+        map["/sys/class/hwmon/hwmon0/temp5_crit"]);
 
     updateSensorValues(ui->cpu4_temp_current, ui->cpu4_temp_min, ui->cpu4_temp_max, ui->cpu4_temp_max_allowed, ui->cpu4_temp_critical,
-        stoi(map["/sys/class/hwmon/hwmon0/temp6_input"]),
-        stoi(map["/sys/class/hwmon/hwmon0/temp6_max"]),
-        stoi(map["/sys/class/hwmon/hwmon0/temp6_crit"]));
+        map["/sys/class/hwmon/hwmon0/temp6_input"],
+        map["/sys/class/hwmon/hwmon0/temp6_max"],
+        map["/sys/class/hwmon/hwmon0/temp6_crit"]);
 
     updateSensorValues(ui->cpu5_temp_current, ui->cpu5_temp_min, ui->cpu5_temp_max, ui->cpu5_temp_max_allowed, ui->cpu5_temp_critical,
-        stoi(map["/sys/class/hwmon/hwmon0/temp7_input"]),
-        stoi(map["/sys/class/hwmon/hwmon0/temp7_max"]),
-        stoi(map["/sys/class/hwmon/hwmon0/temp7_crit"]));
+        map["/sys/class/hwmon/hwmon0/temp7_input"],
+        map["/sys/class/hwmon/hwmon0/temp7_max"],
+        map["/sys/class/hwmon/hwmon0/temp7_crit"]);
 }
 
 MainWindow::MainWindow(QWidget *parent) :
